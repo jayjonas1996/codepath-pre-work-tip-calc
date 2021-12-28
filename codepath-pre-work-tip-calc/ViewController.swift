d765fd6fa025792a3d2f93c105e02d50dd21e962
Jay Naik <jayjonas1996@gmail.com>
2021-12-28 17:32:46 -0500

    Initial Commit

---
+++ codepath-pre-work-tip-calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codepath-pre-work-tip-calc
+//
+//  Created by Jay Naik on 12/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
