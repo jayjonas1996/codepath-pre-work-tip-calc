ca72b2be35a7fb2721d8357753ce99c60394267b
Jay Naik <jayjonas1996@gmail.com>
2021-12-28 22:22:00 -0500

    changes

--- codepath-pre-work-tip-calc/ViewController.swift
+++ codepath-pre-work-tip-calc/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.tipAmountLabel.text = "$" + String([0.15, 0.18, 0.2][tipSeg.selectedSegmentIndex] * (Double(self.billAmount.text!) ?? 0))
         let tips = [1.15, 1.18, 1.20]
         let billAmount = Double(self.billAmount.text!) ?? 0
-        let totalAmount = Double(billAmount * tips[tipSeg.selectedSegmentIndex] * 100) / 100
+        let totalAmount = round(Double(billAmount * tips[tipSeg.selectedSegmentIndex] * 100)) / 100
         
         self.totalAmount.text = "$" + String(totalAmount)
     }
